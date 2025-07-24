60e0a0aebb0512d933451019b0e3b2991bcf61c2
linhgrr <linh.nbt225732@sis.hust.edu.vn>
2025-07-24 15:46:02 +0700

    chapter_9

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,21 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= User.find_by id: session[:user_id]
+    if (user_id = session[:user_id])
+      user = User.find_by id: user_id
+      if user && session[:session_token] && user.authenticated?(session[:session_token])
+        @current_user ||= user
+      elsif user && session[:session_token].nil?
+        # fallbak trong truong hợp session hien tai khong co session_token
+        @current_user ||= user
+      end
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by id: user_id
+      if user&.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
@@ -12,7 +26,32 @@ module SessionsHelper
   end
 
   def log_out
+    if logged_in?
+      forget(current_user)
+      forget_session(current_user)
+    end
     reset_session
     @current_user = nil
+  end
+
+  def remember user
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget user
+    user.forget
+    cookies.delete :user_id
+    cookies.delete :remember_token
+  end
+
+  def create_session user
+    user.create_session
+    session[:session_token] = user.session_token
+  end
+
+  def forget_session user
+    user.forget_session if user
   end
 end
